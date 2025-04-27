bcd8fad5f6389d0f7c7c4adfb26e7004b74d1414
Sani3281 <rajsani3281@gmail.com>
2025-04-27 20:46:22 +0530

    Update Blockchain-based Passport.sol

--- contract/Blockchain-based Passport.sol
+++ contract/Blockchain-based Passport.sol
@@ -12,6 +12,9 @@ contract PassportID {
     }
 
     mapping(address => Passport) public passports;
+    address[] public passportHolders; // New array to store citizen addresses
+
+    uint256 public totalIssued; // Track total issued passports
 
     constructor() {
         governmentAuthority = msg.sender;
@@ -22,6 +25,11 @@ contract PassportID {
         _;
     }
 
+    event PassportIssued(address indexed citizen);
+    event PassportUpdated(address indexed citizen);
+    event PassportRevoked(address indexed citizen);
+    event AuthorityTransferred(address indexed oldAuthority, address indexed newAuthority);
+
     function issuePassport(
         address citizen,
         string memory fullName,
@@ -30,6 +38,9 @@ contract PassportID {
     ) public onlyAuthority {
         require(!passports[citizen].isIssued, "Passport already issued");
         passports[citizen] = Passport(fullName, nationality, dob, true);
+        passportHolders.push(citizen);
+        totalIssued++;
+        emit PassportIssued(citizen);
     }
 
     function viewPassport(address citizen) public view returns (string memory, string memory, uint256, bool) {
@@ -37,7 +48,6 @@ contract PassportID {
         return (p.fullName, p.nationality, p.dob, p.isIssued);
     }
 
-    // New Function: Update existing passport
     function updatePassport(
         address citizen,
         string memory fullName,
@@ -46,61 +56,42 @@ contract PassportID {
     ) public onlyAuthority {
         require(passports[citizen].isIssued, "Passport not issued yet");
         passports[citizen] = Passport(fullName, nationality, dob, true);
+        emit PassportUpdated(citizen);
     }
 
-    // New Function: Revoke a passport
     function revokePassport(address citizen) public onlyAuthority {
         require(passports[citizen].isIssued, "Passport not issued yet");
         passports[citizen].isIssued = false;
+        totalIssued--;
+        emit PassportRevoked(citizen);
     }
 
-    // New Function: Check if passport is valid
     function isPassportValid(address citizen) public view returns (bool) {
         return passports[citizen].isIssued;
     }
 
-    // New Function: Transfer government authority to new address
     function transferAuthority(address newAuthority) public onlyAuthority {
         require(newAuthority != address(0), "Invalid address");
+        address oldAuthority = governmentAuthority;
         governmentAuthority = newAuthority;
-    }
-}// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract PassportID {
-    address public governmentAuthority;
-
-    struct Passport {
-        string fullName;
-        string nationality;
-        uint256 dob; // Date of birth as UNIX timestamp
-        bool isIssued;
+        emit AuthorityTransferred(oldAuthority, newAuthority);
     }
 
-    mapping(address => Passport) public passports;
-
-    constructor() {
-        governmentAuthority = msg.sender;
+    // New Function: Citizen can request passport revocation
+    function requestPassportCancellation() public {
+        require(passports[msg.sender].isIssued, "No active passport found");
+        passports[msg.sender].isIssued = false;
+        totalIssued--;
+        emit PassportRevoked(msg.sender);
     }
 
-    modifier onlyAuthority() {
-        require(msg.sender == governmentAuthority, "Not authorized");
-        _;
+    // New Function: View total number of active passports
+    function totalActivePassports() public view returns (uint256) {
+        return totalIssued;
     }
 
-    function issuePassport(
-        address citizen,
-        string memory fullName,
-        string memory nationality,
-        uint256 dob
-    ) public onlyAuthority {
-        require(!passports[citizen].isIssued, "Passport already issued");
-        passports[citizen] = Passport(fullName, nationality, dob, true);
-    }
-
-    function viewPassport(address citizen) public view returns (string memory, string memory, uint256, bool) {
-        Passport memory p = passports[citizen];
-        return (p.fullName, p.nationality, p.dob, p.isIssued);
+    // New Function: View all passport holders (Only authority)
+    function getAllPassportHolders() public view onlyAuthority returns (address[] memory) {
+        return passportHolders;
     }
 }
-
